2e5ef5b98c8ffa0a7eba3ab4789174d4d722ba6e
Giovanni Nunes <giovanni.nunes@gmail.com>
2020-02-23 16:34:49 -0300

    Fixes wrog setup made uploading to PyPI.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="tomatic",
     packages=["tomatic"],
-    version="0.1",
+    version="0.1.1",
     license="gpl-3.0",
     description="Tomatic is a library that helps to enable automatic configuration for python programas.",
     long_description=open("README.rst").read(),
